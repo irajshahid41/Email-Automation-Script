033fe27138601bef68a6af040c65e56c02bf283a
Iraj Shahid <irajshahid41@gmail.com>
2026-05-30 23:37:41 +0500

    Update email_service.py

--- email_service.py
+++ email_service.py
@@ -1,51 +1,23 @@
-import os
-import pickle
-import base64
+import smtplib
+import streamlit as st
 from email.mime.text import MIMEText
-
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-
-SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
-
-
-def gmail_authenticate():
-    creds = None
-
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
-            creds = pickle.load(token)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json",
-                SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-
-        with open("token.pickle", "wb") as token:
-            pickle.dump(creds, token)
-
-    return build("gmail", "v1", credentials=creds)
 
 
 def send_email(to, subject, body):
-    service = gmail_authenticate()
+    sender = st.secrets["EMAIL_ADDRESS"]
+    password = st.secrets["EMAIL_PASSWORD"]
 
-    message = MIMEText(body)
-    message["to"] = to
-    message["from"] = "me"
-    message["subject"] = subject
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = sender
+    msg["To"] = to
 
-    raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            server.login(sender, password)
+            server.send_message(msg)
 
-    service.users().messages().send(
-        userId="me",
-        body={"raw": raw}
-    ).execute()
+        return "✅ Email sent successfully"
 
-    return "Email sent successfully"
+    except Exception as e:
+        return f"❌ Error: {e}"
