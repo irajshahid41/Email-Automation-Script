2b6caa2a5c29d6d65c77201ad39e8936ee6be1f8
Iraj Shahid <irajshahid41@gmail.com>
2026-05-31 00:16:02 +0500

    Update email_service.py

--- email_service.py
+++ email_service.py
@@ -25,7 +25,7 @@ def gmail_authenticate():
             flow = InstalledAppFlow.from_client_secrets_file(
                 "credentials.json", SCOPES
             )
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
 
         with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
